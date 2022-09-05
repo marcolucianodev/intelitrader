eba98342f5738fec19253c98f4d35ccad6f8d1ff
marcolucianodev <maurelio.sluciano@gmail.com>
2022-09-05 15:48:29 -0300

    start the project

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace CadastroDeUsuario.Models;
+
+public class User
+{
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int? Age { get; set; }
+    public DateTime CreationDate { get; set; }
+    public int Id { get; set; }
+}
