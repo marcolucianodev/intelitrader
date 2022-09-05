0c501d6b1147e7ffa0059f3da6bff91a08c4a1fa
marcolucianodev <maurelio.sluciano@gmail.com>
2022-09-05 15:50:26 -0300

    update UserServices file

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ public class User
 {
     public string? Name { get; set; }
     public string? Surname { get; set; }
-    public int? Age { get; set; }
+    public int Age { get; set; }
     public DateTime CreationDate { get; set; }
     public int Id { get; set; }
 }
